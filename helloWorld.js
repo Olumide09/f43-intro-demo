ee64352693ba807f2694f606ac8e40bf171b5069
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2023-09-12 17:08:20 -0500

    finished the demo

---
+++ helloWorld.js
@@ -0,0 +1,29 @@
+const firstName = 'Olumide'
+
+let age = 19;
+
+//console.log (firstName , age)
+
+let height; 
+
+let heightinFeet = 5;
+let inches = 8;
+
+height = heightinFeet * 12
+height += inches
+
+// console.log (height)
+
+let isCool = true
+
+let tempLit = `Hi, my name is ${firstName}! I am ${age} and it is ${isCool} that I am cool.`
+//console.log(tempLit)
+
+let score = 122;
+let score2 = 115;
+
+if (score >= score2) {
+    console.log ('first score is bigger')
+} else {
+    console.log ('second is bigger')
+}
